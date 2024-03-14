86e09cb9ab90c2ec3898e7507c1e8c0ad3a3d983
issa2580 <issadiop2580@gmail.com>
2024-03-14 03:12:03 +0000

    config database

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
 /* modules loading */
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
 require("dotenv").config();
 
 /* servers loading */
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+/* Database connected */
+const URL = process.env.MONGODB_URL;
+mongoose
+  .connect(URL, {
+    /*
+        useFindAndModify: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useNewUrlParser: true,
+    */
+  })
+  .then(() => console.log("Database connected"))
+  .catch((err) => console.log("Connexion failed", err));
 
 /* middlewares loading */
 app.get("/", (req, res) => {
