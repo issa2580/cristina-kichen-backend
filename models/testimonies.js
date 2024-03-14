a9bfdae8d188934bc995bcbb64e9c752ae8352a8
issa2580 <issadiop2580@gmail.com>
2024-03-14 03:45:44 +0000

    testimonies model

---
+++ models/testimonies.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const testimoniesSchema = new mongoose.Schema({
+  thumbnail: {
+    type: "string",
+    trim: true,
+    require: true,
+  },
+  fullname: {
+    type: "string",
+    trim: true,
+    require: true,
+  },
+  city: {
+    type: "string",
+    trim: true,
+    require: true,
+  },
+  description: {
+    type: "string",
+    trim: true,
+    require: true,
+  },
+});
+
+const Testimonies = mongoose.model("Testimonies", testimoniesSchema);
+
+module.export = Testimonies;
